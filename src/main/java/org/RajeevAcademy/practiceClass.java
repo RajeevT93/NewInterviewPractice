cb247e8bcdfdecdf0565d6ac9f3971387535a475
Rajeev <telugu.rajeev@gmail.com>
2026-05-14 12:51:15 +0530

    American guy commit

--- src/main/java/org/RajeevAcademy/practiceClass.java
+++ src/main/java/org/RajeevAcademy/practiceClass.java
@@ -4,5 +4,8 @@ public class practiceClass {
 
     public static void main(String[] args) {
         System.out.println("Git add");
+
+        System.out.println("Git add fro Other");
+        System.out.println("Git add from US");
     }
 }
