8ed0e8dcacb0a750f7494000a2a4a99b49f2bb9f
Rajeev <telugu.rajeev@gmail.com>
2026-05-14 13:14:58 +0530

    Architect Asian Guy

--- src/main/java/org/RajeevAcademy/practiceClass.java
+++ src/main/java/org/RajeevAcademy/practiceClass.java
@@ -8,4 +8,18 @@ public class practiceClass {
         System.out.println("Git add fro Other");
         System.out.println("Git add from US");
     }
+
+    public void getData(String str){
+
+        System.out.println("Get Data from branch Develop");
+    }
+    public void getDataString(String str){
+
+        System.out.println("Get Data from branch Develop");
+    }
+
+    public void getDataInt(int num){
+
+        System.out.println("Get int Data from branch Develop");
+    }
 }
